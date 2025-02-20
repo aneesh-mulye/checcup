b3eca0ad8948716a694a428d72a98558d716f627
Aneesh Mulye <851233+aneesh-mulye@users.noreply.github.com>
2025-02-20 18:17:13 +0530

    Base generators

--- src/checcup/core.clj
+++ src/checcup/core.clj
@@ -1,6 +1,44 @@
-(ns checcup.core)
+(ns checcup.core
+  (:require [clojure.test.check.generators :as gen]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def base-gens
+  #{:nat
+    :small-integer
+    :large-integer
+    :double
+    :size-bounded-bigint
+    :boolean
+    :byte
+    :bytes
+    :char
+    :char-ascii
+    :char-alphanumeric
+    :char-alpha
+    :string
+    :string-ascii
+    :string-alphanumeric
+    :keyword
+    :keyword-ns
+    :symbol
+    :symbol-ns
+    :ratio
+    :big-ratio
+    :uuid
+    :simple-type
+    :simple-type-printable
+    :simple-type-equatable
+    :simple-type-printable-equatable
+    :any
+    :any-printable
+    :any-equatable
+    :any-printable-equatable})
+
+(defn gen [checcup]
+  (cond
+    (and (keyword? checcup) (not (base-gens checcup)))
+    (throw (ex-info "I don't know how to convert that to a generator." {:value checcup}))
+
+    (keyword? checcup) @(ns-resolve 'clojure.test.check.generators (symbol (name checcup)))
+
+    :else
+    (throw (ex-info "I don't know how to convert that to a generator." {:value checcup}))))
--- test/checcup/core_test.clj
+++ test/checcup/core_test.clj
@@ -1,7 +1,51 @@
 (ns checcup.core-test
-  (:require [clojure.test :refer :all]
-            [checcup.core :refer :all]))
+  (:require [checcup.core :refer :all]
+            [expectations.clojure.test
+             :refer
+             [defexpect expect expecting]]
+            [clojure.test.check.generators :as gen]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defexpect base-gens-test
+  (expecting "numbers"
+             (expect gen/nat (gen :nat))
+             (expect gen/small-integer (gen :small-integer))
+             (expect gen/large-integer (gen :large-integer))
+             (expect gen/double (gen :double))
+             (expect gen/size-bounded-bigint (gen :size-bounded-bigint)))
+  (expecting "boolean"
+             (expect gen/boolean (gen :boolean)))
+  (expecting "byte[s]"
+             (expect gen/byte (gen :byte))
+             (expect gen/bytes (gen :bytes)))
+  (expecting "char and chars"
+             (expect gen/char (gen :char))
+             (expect gen/char-ascii (gen :char-ascii))
+             (expect gen/char-alphanumeric (gen :char-alphanumeric))
+             (expect gen/char-alpha (gen :char-alpha)))
+  (expecting "strings"
+             (expect gen/string (gen :string))
+             (expect gen/string-ascii (gen :string-ascii))
+             (expect gen/string-alphanumeric (gen :string-alphanumeric)))
+  (expecting "keywords"
+             (expect gen/keyword (gen :keyword))
+             (expect gen/keyword-ns (gen :keyword-ns)))
+  (expecting "symbols"
+             (expect gen/symbol (gen :symbol))
+             (expect gen/symbol-ns (gen :symbol-ns)))
+  (expecting "ratios"
+             (expect gen/ratio (gen :ratio))
+             (expect gen/big-ratio (gen :big-ratio)))
+  (expecting "uuid"
+             (expect gen/uuid (gen :uuid)))
+  (expecting "simple types"
+             (expect gen/simple-type (gen :simple-type))
+             (expect gen/simple-type-printable (gen :simple-type-printable))
+             (expect gen/simple-type-equatable (gen :simple-type-equatable))
+             (expect gen/simple-type-printable-equatable
+                     (gen :simple-type-printable-equatable)))
+  (expecting "any"
+             (expect gen/any (gen :any))
+             (expect gen/any-printable (gen :any-printable))
+             (expect gen/any-equatable (gen :any-equatable))
+             (expect gen/any-printable-equatable
+                     (gen :any-printable-equatable))))
